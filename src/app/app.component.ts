0d3ba392a0df65f61dc2e2c0956c9027f27f542c
Gion Kunz <Gion_Kunz@rcomext.com>
2021-04-20 10:41:35 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+interface Point {
+  x: number;
+  y: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  name = 'Peter';
+  seconds = 0;
+  mouseX = 0;
+  mouseY = 0;
+  numbers: number[] = [];
+  points: Point[] = [];
+
+  constructor() {
+    setInterval(() => this.numbers.push(++this.seconds), 1000);
+  }
+
+  reset(): void {
+    this.seconds = 0;
+    this.numbers = [];
+  }
+
+  updateMousePosition(event: MouseEvent): void {
+    this.mouseX = event.offsetX;
+    this.mouseY = event.offsetY;
+  }
+
+  addPoint(): void {
+    this.points.push({
+      x: this.mouseX,
+      y: this.mouseY
+    });
+  }
 }
